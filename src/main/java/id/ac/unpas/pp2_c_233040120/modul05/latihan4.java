e521fa913d7767e8897574fcaad47e963cf48e45
fitriyani20 <fitriyani.233040120@mail.unpas.ac.id>
2025-10-29 11:58:55 +0700

    mengerjakan tugas

---
+++ src/main/java/id/ac/unpas/pp2_c_233040120/modul05/latihan4.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package id.ac.unpas.pp2_c_233040120.modul05;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import javax.swing.*;
+
+/**
+ *
+ * @author Fitriyani Rahmadini
+ */
+public class latihan4 {
+     public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run(){
+                JFrame frame = new JFrame("Contoh BorderLayout");
+                frame.setSize(400, 300);
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                
+                frame.setLayout(new BorderLayout());
+                
+                JLabel label = new JLabel("Label ada di Atas (NORTH) ");
+                JButton button = new JButton("Tombol ada di Bawah (SHOUTH) ");
+                
+                
+                button.addActionListener(e -> {
+                    label.setText("Tombol di SOUTH diklik!"); 
+                } ); 
+                
+                frame.add(label, BorderLayout.NORTH);
+                frame.add(button, BorderLayout.SOUTH);
+                
+                frame.add(new JButton("WEST"), BorderLayout.WEST);
+                frame.add(new JButton("EAST"), BorderLayout.EAST);
+                frame.add(new JButton("CENTER"), BorderLayout.CENTER);
+                
+                frame.setVisible(true);  
+               
+            }  
+        } );
+    }
+}
+
